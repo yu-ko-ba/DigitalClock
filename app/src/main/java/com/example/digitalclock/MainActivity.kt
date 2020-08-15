2e3e7c0b69207a3022afa632c3c8f1feee8e41ca
yu-ko-ba <lily_lime_mint@outlook.com>
2020-08-15 15:42:22 +0900

    コメントを追加した

--- app/src/main/java/com/example/digitalclock/MainActivity.kt
+++ app/src/main/java/com/example/digitalclock/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.digitalclock
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
+//import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
@@ -9,6 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.timer
 
+// Activityクラスを継承すると、上の方にアプリ名が出なくなる
 //class MainActivity : AppCompatActivity() {
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +17,23 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
         // アプリをフルスクリーンで起動するための処理
-        val decorView = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
 
         // 現在時刻を表示する処理
+        // Handlerクラスのインスタンスを取得しておく
         val handler = Handler()
+        // 0.1秒ごとに現在時刻を取得する
         timer(period = 100) {
+            // 現在時刻を取得する
             val calendar = Calendar.getInstance()
+            // 時間を取り出す（24時間表記）
             val hour = String.format("%02d", calendar.get(Calendar.HOUR_OF_DAY))
+            // 分を取り出す
             val minute = String.format("%02d", calendar.get(Calendar.MINUTE))
 
+            // 画面の表示はメインスレッドからしか変えられないから、メインスレッドで実行する
             handler.post {
+                // IDが time のtextViewの表示する文字として hour定数の中身 + ":" + minute定数の中身 をセットする
                 time.text = "${hour}:${minute}"
             }
         }
